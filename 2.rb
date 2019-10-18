d43255522fee686e1dceb0308e6d10f3bd4d1d10
Darren Johnson <djohnson@geminiduplication.com>
2019-10-17 20:23:57 -0700

    few more spaces

--- 2.rb
+++ 2.rb
@@ -1,2 +1,3 @@
 a=0
-$<.read.split.map{|w|p w if a==1||a==0&&w[2]==?z && a=1;w[3]&&!([w[3]]&%w[a e i o u])[0]&&a=0}
+$<.read.split.map{|w|p w if a==1||a==0&&w[2]==?z&&a=1
+w[3]&&!([w[3]]&%w[a e i o u])[0]&&a=0}
